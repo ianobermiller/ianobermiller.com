8904265876b7054751e5e1f53cbfd1a6cce9b939
Ian Obermiller <ian@obermillers.com>
2019-09-15 21:14:49 -0700

    Convert to hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,30 @@
-import * as React from 'react'
-import Link from 'gatsby-link'
+import * as React from 'react';
+import Link from 'gatsby-link';
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
-interface IndexPageProps {
+interface Props {
   data: {
     site: {
       siteMetadata: {
-        title: string
-      }
-    }
-  }
+        title: string;
+      };
+    };
+  };
 }
 
-export default class extends React.Component<IndexPageProps, {}> {
-  constructor(props: IndexPageProps, context: any) {
-    super(props, context)
-  }
-  public render() {
-    return (
-      <div>
-        <h1>Hi people</h1>
-        <p>
-          Welcome to your new{' '}
-          <strong>{this.props.data.site.siteMetadata.title}</strong> site.
-        </p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </div>
-    )
-  }
+export default function(props: Props): React.FC<Props> {
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>
+        Welcome to your new{' '}
+        <strong>{props.data.site.siteMetadata.title}</strong> site.
+      </p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </div>
+  );
 }
 
 export const pageQuery = graphql`
@@ -40,4 +35,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
