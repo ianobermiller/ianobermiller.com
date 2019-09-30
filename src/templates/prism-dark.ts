41b4e4914fc1e911852497a7489d2f87307a3b97
Ian Obermiller <ian@obermillers.com>
2019-09-30 00:02:34 -0700

    Clean up code theming

--- src/templates/prism-dark.ts
+++ src/templates/prism-dark.ts
@@ -10,20 +10,9 @@ export default css`
   code[class*='language-'],
   pre[class*='language-'] {
     color: white;
-    background: none;
     text-shadow: 0 -0.1em 0.2em black;
-    font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
-    font-size: 1em;
-    text-align: left;
-    white-space: pre;
-    word-spacing: normal;
-    word-break: normal;
-    word-wrap: normal;
+    white-space: pre-wrap;
     line-height: 1.5;
-
-    -moz-tab-size: 4;
-    -o-tab-size: 4;
-    tab-size: 4;
 
     -webkit-hyphens: none;
     -moz-hyphens: none;
@@ -38,26 +27,13 @@ export default css`
     }
   }
 
-  pre[class*='language-'],
-  :not(pre) > code[class*='language-'] {
-    background: hsl(30, 20%, 25%);
-  }
 
-  /* Code blocks */
-  pre[class*='language-'] {
-    padding: 1em;
-    margin: 0.5em 0;
-    overflow: auto;
-    border: 0.3em solid hsl(30, 20%, 40%);
-    border-radius: 0.5em;
-    box-shadow: 1px 1px 0.5em black inset;
-  }
 
   /* Inline code */
   :not(pre) > code[class*='language-'] {
     padding: 0.15em 0.2em 0.05em;
-    border-radius: 0.3em;
-    border: 0.13em solid hsl(30, 20%, 40%);
+    border-radius: 4px;
+    border: dotted 1px #f1ac7d;
     box-shadow: 1px 1px 0.3em -0.1em black inset;
     white-space: normal;
   }
--- src/templates/prism-light.ts
+++ src/templates/prism-light.ts
@@ -10,41 +10,14 @@ export default css`
   code[class*='language-'],
   pre[class*='language-'] {
     color: black;
-    background: none;
     text-shadow: 0 1px white;
-    font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
-    font-size: 1em;
-    text-align: left;
-    white-space: pre;
-    word-spacing: normal;
-    word-break: normal;
-    word-wrap: normal;
+    white-space: pre-wrap;
     line-height: 1.5;
-
-    -moz-tab-size: 4;
-    -o-tab-size: 4;
-    tab-size: 4;
 
     -webkit-hyphens: none;
     -moz-hyphens: none;
     -ms-hyphens: none;
     hyphens: none;
-  }
-
-  pre[class*='language-']::-moz-selection,
-  pre[class*='language-'] ::-moz-selection,
-  code[class*='language-']::-moz-selection,
-  code[class*='language-'] ::-moz-selection {
-    text-shadow: none;
-    background: #b3d4fc;
-  }
-
-  pre[class*='language-']::selection,
-  pre[class*='language-'] ::selection,
-  code[class*='language-']::selection,
-  code[class*='language-'] ::selection {
-    text-shadow: none;
-    background: #b3d4fc;
   }
 
   @media print {
@@ -54,22 +27,11 @@ export default css`
     }
   }
 
-  /* Code blocks */
-  pre[class*='language-'] {
-    padding: 1em;
-    margin: 0.5em 0;
-    overflow: auto;
-  }
-
-  :not(pre) > code[class*='language-'],
-  pre[class*='language-'] {
-    background: #f5f2f0;
-  }
-
   /* Inline code */
   :not(pre) > code[class*='language-'] {
-    padding: 0.1em;
-    border-radius: 0.3em;
+    padding: 0.15em 0.2em 0.05em;
+    border-radius: 4px;
+    border: dotted 1px #f1ac7d;
     white-space: normal;
   }
 
@@ -113,7 +75,6 @@ export default css`
   .language-css .token.string,
   .style .token.string {
     color: #9a6e3a;
-    background: hsla(0, 0%, 100%, 0.5);
   }
 
   .token.atrule,
