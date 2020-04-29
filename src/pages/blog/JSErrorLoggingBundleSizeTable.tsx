8d3fd7f884586671edd38bd87626e4de06699820
Ian Obermiller <ian@obermillers.com>
2020-04-29 06:12:26 -0700

    Add post comparing JS Error Logging Services

---
+++ src/pages/blog/JSErrorLoggingBundleSizeTable.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import React from "react";
+import data from "./js-error-logging-bundle-size.json";
+
+export default function JSErrorLoggingBundleSizeTable() {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Source</th>
+          <th>Min+Gzip kB</th>
+          <th>Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(entry => (
+          <tr>
+            <td>
+              <a href={entry.url}>{entry.name}</a>
+            </td>
+            <td>
+              {entry.source.startsWith("http") ? (
+                <a href={entry.source}>{entry.source}</a>
+              ) : (
+                <pre>{entry.source}</pre>
+              )}
+            </td>
+            <td>{entry.minzipSizeInKB}</td>
+            <td>{new Date(entry.lastUpdated * 1000).toLocaleDateString()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+const Table = styled.table`
+  th {
+    font-weight: bold;
+    text-align: left;
+    white-space: nowrap;
+  }
+
+  th,
+  td {
+    &:not(:last-child) {
+      padding-right: 16px;
+    }
+  }
+`;
