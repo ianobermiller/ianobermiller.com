b570cf569fe8f4fc3c4d348219ed04f86c16a1ee
Ian Obermiller <ian@obermillers.com>
2020-08-29 21:10:21 -0700

    Update lint for ES2020

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,4 +15,14 @@ module.exports = {
       },
     ],
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  env: {
+    es2020: true,
+  },
 };
