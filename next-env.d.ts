ed7ee40f23efed298675d4d428ea166a35b9b0d0
Ian Obermiller <ian@obermillers.com>
2021-04-07 23:15:55 -0700

    Add config for images

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next-images" />
 
 declare module '*.scss' {
   const classes: {[key: string]: string};
