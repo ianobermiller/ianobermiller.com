5b7f7a7d8ba0f626c1ad802067c8b843e8c32a8e
Ian Obermiller <ian@obermillers.com>
2021-08-31 10:47:46 -0400

    Add description and location to Nocatee events

--- pages/api/nocatee-events.api.ts
+++ pages/api/nocatee-events.api.ts
@@ -1,33 +1,77 @@
+import type {VercelRequest, VercelResponse} from '@vercel/node';
 import ical from 'ical-generator';
 import fetch from 'node-fetch';
+import {FormData} from 'formdata-node';
+import cheerio from 'cheerio';
+import {OPENSSL_VERSION_NUMBER} from 'node:constants';
 
 const BASE_URL = 'https://residents.nocatee.com';
 
-export default async function main(req, res) {
-  const calendarHtml = await (
-    await fetch(`${BASE_URL}/events?type=calendar`)
-  ).text();
+export default async function main(
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const day = today.getDate();
+  const form = new FormData();
+  form.set('ctl00$content$from_date', `${month}/${day}/${year}`);
+  form.set('ctl00$content$to_date', `${month}/${day}/${year + 1}`);
 
-  const eventsJson = calendarHtml.match(/events:(.*),/)?.[1];
+  const [calendarHtml, calendarHTMLWithJson] = await Promise.all(
+    [
+      fetch(`${BASE_URL}/events?category_no=5116`, {
+        method: 'POST',
+        body: form as any,
+      }),
+      fetch(`${BASE_URL}/events?category_no=5116&type=calendar`),
+    ].map(f => f.then(res => res.text())),
+  );
 
+  const eventsJson = calendarHTMLWithJson.match(/events:(.*),/)?.[1];
   if (!eventsJson) {
     throw new Error('Could not find events json');
   }
 
-  const events = JSON.parse(eventsJson);
+  const jsonEvents = JSON.parse(eventsJson);
 
-  const calendar = ical({name: 'Nocatee Events'});
-  calendar.timezone('America/New_York');
+  const $ = cheerio.load(calendarHtml);
+  const htmlEvents = $('.eventBlock');
 
-  for (const event of events) {
+  const calendar = ical({
+    name: 'Nocatee Events',
+    timezone: 'America/New_York',
+    source: 'https://www.nocatee.com/events',
+  });
+
+  for (const htmlEvent of htmlEvents) {
+    if (htmlEvent.type !== 'tag') continue;
+
+    const id = htmlEvent.attribs.id.split('-')[1];
+    const jsonEvent = jsonEvents.find(e => e.id === id);
+    if (!jsonEvent) continue;
+
+    const description = $('a + .txt', htmlEvent).text().trim();
+    const venue = $('.event-venue-text', htmlEvent).text().trim();
+    let address = $('.event-location-text', htmlEvent).text().trim();
+    if (address.startsWith(venue)) {
+      address = address.slice(venue.length).trim();
+    }
+
     calendar.createEvent({
-      id: event.id,
-      start: event.start,
-      end: event.end,
-      summary: event.title,
-      url: BASE_URL + event.url,
+      id,
+      start: jsonEvent.start,
+      end: jsonEvent.end,
+      summary: jsonEvent.title,
+      url: BASE_URL + jsonEvent.url,
+      location: {
+        title: venue,
+        address,
+      },
+      description,
     });
   }
 
-  calendar.serve(res);
+  calendar.serve(response);
 }
