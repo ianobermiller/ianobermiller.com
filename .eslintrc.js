d9db66adfa007d005be055555afdb874539dcfa3
Ian Obermiller <ian@obermillers.com>
2022-10-15 17:10:38 -0400

    Remove eslint-plugin-graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  plugins: ['graphql'],
-  rules: {
-    'graphql/template-strings': [
-      'error',
-      {
-        env: 'relay',
-        tagName: 'graphql',
-        schemaJsonFilepath: path.resolve(
-          __dirname,
-          'src/__generated__gatsby-introspection.json',
-        ),
-      },
-    ],
-  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
