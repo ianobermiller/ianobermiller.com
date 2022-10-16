af4ec91380d61d45579168daf4c5c4bab24e5273
Ian Obermiller <ian@obermillers.com>
2022-10-15 22:16:35 -0400

    Remove comments with utterances, since it was rendering twice and no one has used it :)

--- layouts/Post.tsx
+++ layouts/Post.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement, useEffect, useRef} from 'react';
+import React, {ReactElement} from 'react';
 import DateText from './DateText';
 import Layout from './Layout';
 
@@ -21,26 +21,6 @@ export default function Post({children, date, title}: Props): ReactElement {
         </DateText>
         <div className="e-content markdown">{children}</div>
       </article>
-      <Comments />
     </Layout>
   );
-}
-
-function Comments() {
-  const ref = useRef<HTMLDivElement | null>(null);
-  useEffect(() => {
-    const scriptElement = document.createElement('script');
-    scriptElement.async = true;
-    scriptElement.crossOrigin = 'anonymous';
-    scriptElement.src = 'https://utteranc.es/client.js';
-
-    scriptElement.setAttribute('issue-term', 'pathname');
-    scriptElement.setAttribute('label', 'comment');
-    scriptElement.setAttribute('repo', 'ianobermiller/ianobermiller.com');
-    scriptElement.setAttribute('theme', 'preferred-color-scheme');
-
-    ref.current?.appendChild(scriptElement);
-  }, []);
-
-  return <div ref={ref} />;
 }
